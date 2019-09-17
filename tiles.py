8d39dbf9a1a4349d70d2b5441c80d8d3101aa894
unknown <ragnhildurg19@ru.is>
2019-09-17 11:08:50 +0000

    hjálp frá Stulla

--- tiles.py
+++ tiles.py
@@ -1,3 +1,7 @@
+def helperFunction(xCoordinates, yCoordinates, letter):
+    
+
+
 # Playing the Tile game
 n = str("(N)orth")
 e = str("(E)ast")
@@ -5,5 +9,16 @@ s = str("(S)outh")
 w = str("(W)est")
 
 
-print("You can travel: " +n)
-user_input = input("Direction: ")
+
+
+while True:
+    xCoordinates = 1
+    yCoordinates = 1
+
+    user_input = input("Direction: ")
+    user_input = user_input.toLower()
+    if (user_input == 'n' || ...):
+        #call a function here
+
+    else:
+        print('Not a valid direction')
